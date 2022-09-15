97a3e3424660d9cf902ebc5cbdf6ba9bb581ec87
gbehs.ext@alpargatas.com <Sensedia@2021>
2022-09-15 16:25:17 -0300

    feat: first project commit

---
+++ src/main/java/br/com/guilhermebehs/ftgo/FoodToGoApplication.java
@@ -0,0 +1,13 @@
+package br.com.guilhermebehs.ftgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodToGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodToGoApplication.class, args);
+	}
+
+}
