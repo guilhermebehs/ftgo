ed1ca763007c284bb3aa26c9299ff9ed693fc46a
gbehs.ext@alpargatas.com <Sensedia@2021>
2022-09-29 11:33:09 -0300

    chore: adjust Eureka config

--- kitchen/src/main/java/br/com/guilhermebehs/kitchen/KitchenMsApplication.java
+++ kitchen/src/main/java/br/com/guilhermebehs/kitchen/KitchenMsApplication.java
@@ -2,9 +2,10 @@ package br.com.guilhermebehs.kitchen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class KitchenMsApplication {
 
 	public static void main(String[] args) {
